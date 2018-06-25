e9c5c84a1c1c71e18f4860caabad249c863e02c7
Brandon Harris <bharris@allegient.com>
2018-06-25 16:27:44 -0400

    Search Heroes functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,10 @@ import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { InMemoryDataService } from './in-memory-data.service';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
-  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent],
+  declarations: [AppComponent, HeroesComponent, HeroDetailComponent, MessagesComponent, DashboardComponent, HeroSearchComponent],
   imports: [
     BrowserModule,
     FormsModule,
