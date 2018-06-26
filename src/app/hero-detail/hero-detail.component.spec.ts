9923097cf10d39d07310bccb7b40119357ab41c1
Brandon Harris <bharris@allegient.com>
2018-06-26 08:44:25 -0400

    Fix HeroDetailComponent test - Location + Params

--- src/app/hero-detail/hero-detail.component.spec.ts
+++ src/app/hero-detail/hero-detail.component.spec.ts
@@ -1,20 +1,27 @@
 import { Location } from '@angular/common';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { SpyLocation } from '@angular/common/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { of, Observable } from 'rxjs';
+import { HeroService } from '../hero.service';
 import { HeroDetailComponent } from './hero-detail.component';
+import { Hero } from '../hero';
 
 describe('HeroDetailComponent', () => {
   let component: HeroDetailComponent;
   let fixture: ComponentFixture<HeroDetailComponent>;
   let loc: Location;
+  let heroService: HeroService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [FormsModule, HttpClientTestingModule],
+      imports: [
+        FormsModule,
+        HttpClientTestingModule,
+        RouterTestingModule.withRoutes([{ path: '', component: HeroDetailComponent }])
+      ],
       declarations: [HeroDetailComponent],
       providers: [
         {
@@ -30,11 +37,20 @@ describe('HeroDetailComponent', () => {
     component = fixture.componentInstance;
 
     loc = TestBed.get(Location);
+    heroService = TestBed.get(HeroService);
 
     fixture.detectChanges();
   });
 
   it('should create', () => {
+    spyOn(heroService, 'getHero').and.returnValue(getMockHero());
     expect(component).toBeTruthy();
   });
+
+  function getMockHero(): Observable<Hero> {
+    return of(<Hero>{
+      id: 17,
+      name: 'Test Hero'
+    });
+  }
 });
